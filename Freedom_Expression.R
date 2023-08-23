7f2ddfddd6efb783c25f145dd6cc6f793e6b1e22
aidar-zinnatullin <aidar.zinnatulla@gmail.com>
2023-08-23 12:44:25 +0400

    some other things added

--- Freedom_Expression.R
+++ Freedom_Expression.R
@@ -1,3 +1,48 @@
+
+### Freedom of Expression and Alternative Sources of Information index according V-Dem
+
+# Question: To what extent does government respect press and media freedom, the freedom of ordinary people to 
+# discuss political matters at home and in the public sphere, as well as the freedom of academic and 
+# cultural expression?
+# Clarification: This index includes all variables in the two indices v2x_freexp and v2xme_altinf.
+# Scale: Interval, from low to high (0-1).
+# Source(s): v2mecenefm v2meharjrn v2meslfcen v2xcl_disc v2clacfree v2mebias v2mecrit v2merange
+# Data release: 4-13.
+# Aggregation: The index is formed by taking the point estimates from a Bayesian factor analysis model of the 
+# indicators for media censorship effort (v2mecenefm), harassment of journalists (v2meharjrn), media bias (v2mebias), 
+# media self-censorship (v2meslfcen), print/broadcast
+# media critical (v2mecrit), and print/broadcast media perspectives (v2merange), 
+# freedom of discussion for men/women (v2cldiscm, v2cldiscw), and freedom of academic and cultural 
+# expression (v2clacfree).
+
+
+
+# sessionInfo()
+# R version 4.2.2 (2022-10-31)
+# Platform: aarch64-apple-darwin20 (64-bit)
+# Running under: macOS Ventura 13.4.1
+# 
+# Matrix products: default
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.2-arm64/Resources/lib/libRlapack.dylib
+# 
+# locale:
+#   [1] ru_RU.UTF-8/ru_RU.UTF-8/ru_RU.UTF-8/C/ru_RU.UTF-8/ru_RU.UTF-8
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] ggthemes_4.2.4  lubridate_1.9.2 forcats_1.0.0   stringr_1.5.0   dplyr_1.1.2     purrr_1.0.1     readr_2.1.4    
+# [8] tidyr_1.3.0     tibble_3.2.1    ggplot2_3.4.2   tidyverse_2.0.0 here_1.0.1     
+# 
+# loaded via a namespace (and not attached):
+#   [1] pillar_1.9.0     compiler_4.2.2   tools_4.2.2      lifecycle_1.0.3  gtable_0.3.3     timechange_0.2.0
+# [7] pkgconfig_2.0.3  rlang_1.1.1      cli_3.6.1        rstudioapi_0.14  withr_2.5.0      generics_0.1.3  
+# [13] vctrs_0.6.3      hms_1.1.3        rprojroot_2.0.3  grid_4.2.2       tidyselect_1.2.0 glue_1.6.2      
+# [19] R6_2.5.1         fansi_1.0.4      farver_2.1.1     tzdb_0.4.0       magrittr_2.0.3   scales_1.2.1    
+# [25] colorspace_2.1-0 labeling_0.4.2   utf8_1.2.3       stringi_1.7.12   munsell_0.5.0    crayon_1.5.2 
+
+
 library(here)
 library(tidyverse)
 library(ggthemes)
